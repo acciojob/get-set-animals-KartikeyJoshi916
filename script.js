b7d6e85f26f206831fe92f33ad469c68cf7b4fff
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2024-12-27 11:45:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
 class Animal {
-  constructor(species) {
-    this._species = species;
-  }
-
-  get species() {
-    return this._species;
-  }
-
-  makeSound() {
-    ;
-  }
+ 
+	constructor(species){
+		this._species=species;
+	}
+ 
+	get species(){
+		return this._species;
+	}
+	makeSound(){
+		console.log(`The ${this.species} makes a sound`)
+	}
 }
-
+ 
 class Dog extends Animal {
-  constructor(species) {
-    super(species);
-  }
-
-  bark() {
-    console.log("woof");
-  }
+	bark(){
+		console.log("woof");
+	}
 }
-
+ 
 class Cat extends Animal {
-  constructor(species) {
-    super(species);
-  }
-
-  purr() {
-    console.log("purr");
-  }
+	purr(){
+		console.log("purr");
+	}
 }
-
+ 
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
